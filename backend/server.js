2ec1be831bd3b4609f68087edcdfc11afbbd237c
Vicky Singh <vickysingh7998@gmail.com>
2024-11-26 02:24:02 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 dotenv.config()
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //To parse incoming requests with Json payloads
 app.use(cookieParser());
 
@@ -19,9 +22,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-// app.get("/", (req,res) => {
-//     res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
+
 
 server.listen(PORT,() => {
     connectToMongoDB();
